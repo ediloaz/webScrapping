4257f6bae0e1390ca0812cf7f5617f003df2641e
Edisson <ediloaz@gmail.com>
2024-07-04 14:18:29 -0600

    saving in s3 the images

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Lista de Tareas
 
-- [ ] Remover el enlace de las imágenes.
+## Imágenes
+- [x] Remover el enlace de las imágenes.
+- [ ] Subirlas conforme se van descargando.
+- [ ] Definir la estructura y flujo de cómo manejar estas imágenes (Considerar la posibilidad de carga diferida desde otro sitio gratuito (Google Drive, por ejemplo)).
 - [ ] Hacer público los datos al utilizar la página.
-- [ ] Guardar estas imágenes en nuestra propia base de datos.
+
+
+## JSONs
 - [ ] Crear MultiJsons para manejarlos en un sitio web estático.
+
+
+## WebSite
+- [ ] Primera versión del WebSite, muestra un listado de todos los perfumes del JSON.
+
+
+## Mejor continúa
+- [ ] Obtener los demás atributos.
 - [ ] Observar los datos y detectar patrones.
 - [ ] Conectar con Inteligencia Artificial para, mediante un trabajo programado, mejorar periódicamente la descripción, detalles o atributos de los perfumes.
-- [ ] Obtener los demás atributos.
 - [ ] Conectar con otras páginas y fusionar datos de los mismos perfumes.
 - [ ] Resolver cómo conseguir imágenes de alta calidad.
-    - Considerar la posibilidad de carga diferida desde otro sitio gratuito (Google Drive, por ejemplo).
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import boto3
 import os
 import json
 import codecs
@@ -8,9 +9,13 @@ from dotenv import load_dotenv
 
 path_to_save_sitemap = './sitemaps/most_recent_sitemap.xml'
 sitemap_url = os.getenv('SITEMAP_URL')
+bucket_name = os.getenv('bucket_name')
+data_file = './data/perfumes.json'
 
 USE_LOCAL_XML = True
-CANTIDAD_MAXIMA_PRODUCTOS = 500
+CANTIDAD_MAXIMA_PRODUCTOS = 2
+
+BUILD_NEW_JSON = False
 
 def get_product_urls_from_sitemap(sitemap_url):
     response = requests.get(sitemap_url)
@@ -44,68 +49,122 @@ def obtener_precio(string_precio):
     return precio
 
 
+if (BUILD_NEW_JSON):
+    lista_perfumes = [] # Para el archivo JSON
+    product_urls = get_product_urls(sitemap_url)
+    print(f'Se encontraron {len(product_urls)} URLs de productos.')
 
-lista_perfumes = [] # Para el archivo JSON
-product_urls = get_product_urls(sitemap_url)
-print(f'Se encontraron {len(product_urls)} URLs de productos.')
+    # Open each URL and extract the product information
+    cont = 1
+    for url in product_urls:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        script = soup.find('script', type='text/template').text
+        # Eliminar las secuencias de escape
+        script = script.replace('\\n', '').replace('\\t', '').replace('\\"', '"').replace('\"', '"').replace('\\/', '/')
+        script = codecs.decode(script, 'unicode_escape')
+        cleaned_html = html.unescape(script)
 
-# Open each URL and extract the product information
-cont = 1
-for url in product_urls:
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    script = soup.find('script', type='text/template').text
-    # Eliminar las secuencias de escape
-    script = script.replace('\\n', '').replace('\\t', '').replace('\\"', '"').replace('\"', '"').replace('\\/', '/')
-    script = codecs.decode(script, 'unicode_escape')
-    cleaned_html = html.unescape(script)
+        # Quitar las comillas alrededor del HTML
+        cleaned_html = cleaned_html.replace('<script type="text/template">"', '').replace('"</script>', '')
 
-    # Quitar las comillas alrededor del HTML
-    cleaned_html = cleaned_html.replace('<script type="text/template">"', '').replace('"</script>', '')
+        # Parsear como HTML con BeautifulSoup
+        soup = BeautifulSoup(cleaned_html, 'html.parser')
 
-    # Parsear como HTML con BeautifulSoup
-    soup = BeautifulSoup(cleaned_html, 'html.parser')
+        perfume = {
+            'id': '',
+            'titulo': '',
+            'precio_actual': '',
+            'precio_anterior': '',
+            'descripcion': '',
+            'imagenes': []
+        }
 
-    perfume = {
-        'id': '',
-        'titulo': '',
-        'precio_actual': '',
-        'precio_anterior': '',
-        'descripcion': '',
-        'imagenes': []
-    }
+        # ATRIBUTO -> ID
+        id = soup.find('button', class_='single_add_to_cart_button')
+        perfume['id'] = id['value'] if id else 'Sin ID'
 
-    # ATRIBUTO -> ID
-    id = soup.find('button', class_='single_add_to_cart_button')
-    perfume['id'] = id['value'] if id else 'Sin ID'
+        # ATRIBUTO -> TITULO
+        titulo = soup.find('h2', class_='product_title entry-title show-product-nav')
+        perfume['titulo'] = titulo.text if titulo else 'Sin título'
 
-    # ATRIBUTO -> TITULO
-    titulo = soup.find('h2', class_='product_title entry-title show-product-nav')
-    perfume['titulo'] = titulo.text if titulo else 'Sin título'
+        # ATRIBUTO -> PRECIO
+        prices = soup.find_all('span', class_='woocommerce-Price-amount amount')
+        perfume['precio_actual'] = obtener_precio(prices[0].text) if prices else 'No price'
+        if (len(prices) > 1):
+            perfume['precio_anterior'] = obtener_precio(prices[1].text) if prices else 'No price'
 
-    # ATRIBUTO -> PRECIO
-    prices = soup.find_all('span', class_='woocommerce-Price-amount amount')
-    perfume['precio_actual'] = obtener_precio(prices[0].text) if prices else 'No price'
-    if (len(prices) > 1):
-        perfume['precio_anterior'] = obtener_precio(prices[1].text) if prices else 'No price'
+        # ATRIBUTO -> DESCRIPCIÓN
+        descripcion = soup.find('div', id='tab-description')
+        perfume['descripcion'] = descripcion.text.replace('Descripción', '') if descripcion else 'Sin descripción'
 
-    # ATRIBUTO -> DESCRIPCIÓN
-    descripcion = soup.find('div', id='tab-description')
-    perfume['descripcion'] = descripcion.text.replace('Descripción', '') if descripcion else 'Sin descripción'
+        # ATRIBUTO -> IMÁGENES
+        imagenes = soup.find_all('img', class_='img-responsive')
+        # No incuir las que llevan '150x150' en la URL (son miniaturas)
+        imagenes = [imagen for imagen in imagenes if '150x150' not in imagen['src']]
+        perfume['imagenes'] = [imagen['src'] for imagen in imagenes] if imagenes else 'Sin imágenes'
+        
+        lista_perfumes.append(perfume)
+        print(f'Producto {cont}/{len(product_urls)} procesado.')
+        cont += 1
 
-    # ATRIBUTO -> IMÁGENES
-    imagenes = soup.find_all('img', class_='img-responsive')
-    perfume['imagenes'] = [imagen['src'] for imagen in imagenes] if imagenes else 'Sin imágenes'
-    
-    lista_perfumes.append(perfume)
-    print(f'Producto {cont}/{len(product_urls)} procesado.')
-    cont += 1
 
-nombre_archivo = './data/perfumes.json'
-with open(nombre_archivo, 'w') as f:
-    json.dump(lista_perfumes, f, indent=4)
+    with open(data_file, 'w') as f:
+        json.dump(lista_perfumes, f, indent=4)
 
-print(f'Se ha creado el archivo JSON "{nombre_archivo}" con los datos de los perfumes.')
+    print(f'Se ha creado el archivo JSON "{data_file}" con los datos de los perfumes.')
+
+
+
+
+
+
+
+
+# Función para obtener el ContentType basado en la extensión del archivo
+def get_content_type(file_name):
+    if file_name.endswith('.jpg') or file_name.endswith('.jpeg'):
+        return 'image/jpeg'
+    elif file_name.endswith('.png'):
+        return 'image/png'
+    elif file_name.endswith('.gif'):
+        return 'image/gif'
+    return 'image/jpeg'  # Default to JPEG
+
+
+s3 = boto3.client('s3')
+
+
+
+# Prueba de conexión con S3
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+print('Conectado a S3 correctamente ✅')
+
+
+# Lee el archivo JSON
+with open(data_file, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+for item in data:
+    item_id = item['id']
+    for image_url in item['imagenes']:
+        print(f'Descargando {image_url}...')
+        # Descarga la imagen
+        response = requests.get(image_url)
+        image_data = response.content
+        image_name = image_url.split('/')[-1]
+
+        # Obtener el tipo de contenido
+        content_type = get_content_type(image_name)
+
+        # Crear la clave de S3 incluyendo el ID como subcarpeta
+        s3_key = f"{item_id}/{image_name}"
+
+        # Sube la imagen a S3
+        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=image_data, ContentType=content_type)
+
+        print(f'Subido {image_name} a {bucket_name}')
